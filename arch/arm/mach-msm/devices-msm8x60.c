9bcb388cb3d43742f34985d8fbc3766c648b6651
wzhy90 <wzhy90@gmail.com>
2014-05-29 14:56:07 +0800

    redown GPU frequency to 266MHz

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -664,7 +664,7 @@ static struct msm_bus_vectors grp3d_nominal_high_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2484),
+		.ib = KGSL_CONVERT_TO_MBPS(2008),
 	},
 };
 
@@ -673,7 +673,7 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2976),
+		.ib = KGSL_CONVERT_TO_MBPS(2484),
 	},
 };
 
@@ -810,19 +810,19 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 320000000,
+			.gpu_freq = 266667000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
 		{
-			.gpu_freq = 266667000,
+			.gpu_freq = 228571000,
 			.bus_freq = 3,
-			.io_fraction = 0,
+			.io_fraction = 33,
 		},
 		{
-			.gpu_freq = 228571000,
+			.gpu_freq = 200000000,
 			.bus_freq = 2,
-			.io_fraction = 33,
+			.io_fraction = 100,
 		},
 		{
 			.gpu_freq = 160000000,
