0e15e077ccc595bfe187f138e30e35abf5dfaf20
wzhy90 <wzhy90@gmail.com>
2014-06-01 19:58:12 +0800

    cpufreq: rename smartassV2 to smartass2

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -178,7 +178,7 @@ static int cpufreq_governor_smartass(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_smartass2 = {
-	.name = "smartassV2",
+	.name = "smartass2",
 	.governor = cpufreq_governor_smartass,
 	.max_transition_latency = 9000000,
 	.owner = THIS_MODULE,
@@ -762,7 +762,7 @@ static struct attribute * smartass_attributes[] = {
 
 static struct attribute_group smartass_attr_group = {
 	.attrs = smartass_attributes,
-	.name = "smartassV2",
+	.name = "smartass2",
 };
 
 static int cpufreq_governor_smartass(struct cpufreq_policy *new_policy,
